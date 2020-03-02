afddd4fab047c2a1a04ca669648c34a00ce76cb4
Den4eg <notparsed@gmail.com>
2020-03-03 02:47:06 +0300

    Init
    Signed-off-by: Den4eg <notparsed@gmail.com>

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld />
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+    name: 'Home',
+    components: {
+        HelloWorld
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+img {
+    position: absolute;
+    height: 200px;
+    width: 200px;
+}
+</style>
