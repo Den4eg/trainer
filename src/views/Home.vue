443c9e939787fe9815be7117d099aeb79f158033
Den4eg <notparsed@gmail.com>
2020-03-20 06:58:48 +0300

    Home label animation
    Signed-off-by: Den4eg <notparsed@gmail.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,133 @@
-<template>
-    <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png" />
-        <HelloWorld />
+<template xmlns="http://www.w3.org/1999/html">
+  <div class="wrap">
+    <div class="logo-wrapper logo-img">
+      <img alt="Vue logo" src="../assets/logo.png" />
     </div>
+    <div class="logo-wrapper logo-text">
+      <span class="logo-label" v-for="(letter, index) of labelView" :key="index">
+        <span class="anim" v-if="letter !== ' '">{{letter}}</span>
+        <span v-else>
+          <br />
+        </span>
+      </span>
+      <span class="label-indicator" v-show="indicator"></span>
+    </div>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
-    name: 'Home',
-    components: {
-        HelloWorld
+  name: "Home",
+  data() {
+    return {
+      appLabel: "Keyboard Trainer",
+      labelView: []
+    };
+  },
+  methods: {
+    mainIteratorLabel: (string, array, next) => {
+      function* iter(str) {
+        for (let i = 0; i < str.length; i++) yield str[i];
+      }
+      let iterSelf = iter(string);
+
+      let recurse = setTimeout(function test() {
+        let iterObj = iterSelf.next();
+        if (!iterObj.done) {
+          array.push(iterObj.value);
+          recurse = setTimeout(test, Math.floor(Math.random() * 500));
+        } else {
+          clearTimeout(recurse);
+          if (next) {
+            next();
+          }
+        }
+      }, 100);
     }
+  },
+  computed: {
+    indicator() {
+      return this.labelView.length !== this.appLabel.length ? false : true;
+    }
+  },
+  mounted: function() {
+    this.$nextTick(this.mainIteratorLabel(this.appLabel, this.labelView));
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-img {
+.wrap {
+  z-index: -1;
+  position: relative;
+  width: 300px;
+  height: 300px;
+  .logo-wrapper {
     position: absolute;
-    height: 200px;
-    width: 200px;
+    img {
+      z-index: -1;
+    }
+  }
+  .logo-img {
+    padding: 50px;
+    padding-top: 0;
+  }
+
+  .logo-text {
+    top: 200px;
+    width: 300px;
+    text-align: center;
+    .logo-label {
+      font-family: Ubuntu, sans-serif;
+      font-weight: bold;
+      color: #41b883;
+      font-size: 50px;
+      font-style: unset;
+      text-shadow: 0 0 2px #000000;
+
+      // .anim {
+      //   opacity: 0.5;
+      //   transition: all 0.5s ease;
+      // }
+    }
+    .label-indicator {
+      position: relative;
+      opacity: 0;
+      animation: indicator 1.2s infinite 1s;
+      font-size: 45px;
+      padding-left: 5px;
+      &::after {
+        content: "";
+        position: absolute;
+        top: -7px;
+        right: -15px;
+        width: 6px;
+        height: 100%;
+        color: #35495e;
+        background-color: #35495e;
+      }
+    }
+  }
+  .l-move {
+    opacity: 0;
+  }
+  .l-leave-to {
+    opacity: 0;
+  }
+  .l-enter-to,
+  .l-enter-active {
+    opacity: 1;
+  }
+  @keyframes indicator {
+    0% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 0.8;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
 }
 </style>
